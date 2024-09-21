21c4b923c3ef62d9a9c78895c1a7111a9da7dcd0
Yemane-hadush <119968031+Yemane-hadush@users.noreply.github.com>
2024-09-21 11:38:51 +0300

    Create weather_advice.py

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,7 @@
+wether=input("what is the wether look like today,sunny/rainy/cold:")
+if wether=='sunny':
+    print("weer the t shert")
+elif wether=='rainy':
+    print("don't forgat rain cot and good hoody")
+elif wether=='cold':
+    print('weer the warm coat')    
