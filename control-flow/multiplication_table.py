f13d56c82b6d2de87cc7248ea6f3b0dfa76c4971
Yemane-hadush <119968031+Yemane-hadush@users.noreply.github.com>
2024-09-21 11:54:40 +0300

    Create multiplication_table.py

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,7 @@
+# prompt the user input number
+number=int(input("enter the number you want to multplication"))
+for i in range(1,10):
+    x=number
+    y=i
+    result=x*y
+    print(f"{x}*{y}={result}")
